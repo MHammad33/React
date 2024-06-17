23d1fb23a1228482246ab942a740d311e6ef2454
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-17 22:09:33 +0500

    Added Bg change logic

--- 01_Basic_React_Project/src/App.jsx
+++ 01_Basic_React_Project/src/App.jsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 
 function App() {
+	const [color, setColor] = useState("olive");
+
 	const navLinks = ["Home", "About", "Services", "Contact", "Blog"];
 
 	return (
 		<>
 			<Navbar navLinks={navLinks} />
-			<h1 className="text-3xl bg-green-400 p-2">Hello, React</h1>
+			<div className={`w-full h-screen bg-${color}-500 duration-200`}>
+				<div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+					<div className="flex flex-wrap gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+						{["red", "green", "blue", "yellow", "purple"].map(
+							(colorVal, index) => {
+								console.log(colorVal);
+								return (
+									<button
+										key={index}
+										className={`outline-none px-4 bg-${colorVal}-500 text-white rounded-xl`}
+										onClick={() => setColor(colorVal)}
+									>
+										{colorVal}
+									</button>
+								);
+							}
+						)}
+					</div>
+				</div>
+			</div>
 		</>
 	);
 }
