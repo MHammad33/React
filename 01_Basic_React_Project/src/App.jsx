c92340a32ec68d1d4c4b12700548e37f4b1ffed4
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-17 20:58:23 +0500

    Props

--- 01_Basic_React_Project/src/App.jsx
+++ 01_Basic_React_Project/src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
 
 function App() {
+	const navLinks = ["Home", "About", "Services", "Contact", "Blog"];
+
 	return (
 		<>
-			<Navbar />
+			<Navbar navLinks={navLinks} />
 			<h1 className="text-3xl bg-green-400 p-2">Hello, React</h1>
 		</>
 	);
--- 01_Basic_React_Project/src/components/Navbar.jsx
+++ 01_Basic_React_Project/src/components/Navbar.jsx
@@ -1,31 +1,23 @@
-const Navbar = () => {
+import Proptypes from "prop-types";
+
+const Navbar = ({ navLinks = [] }) => {
 	return (
 		<nav className="flex justify-center space-x-4">
-			<a
-				href="/dashboard"
-				className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"
-			>
-				Home
-			</a>
-			<a
-				href="/team"
-				className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"
-			>
-				Team
-			</a>
-			<a
-				href="/projects"
-				className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"
-			>
-				Projects
-			</a>
-			<a
-				href="/reports"
-				className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"
-			>
-				Reports
-			</a>
+			{navLinks.map((link, index) => (
+				<a
+					key={index}
+					href={`/${link.toLowerCase()}`}
+					className="font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"
+				>
+					{link}
+				</a>
+			))}
 		</nav>
 	);
 };
+
+Navbar.propTypes = {
+	navLinks: Proptypes.arrayOf(Proptypes.string),
+};
+
 export default Navbar;
