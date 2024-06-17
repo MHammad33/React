dfc6a6ea82f5136b9684ec1daed969955a16096a
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-17 18:06:34 +0500

    Basic Custom Library Setup

---
+++ Custom_React_Library/src/main.js
@@ -0,0 +1,23 @@
+function customRender(reactElement, root) {
+  const domElement = document.createElement(reactElement.type);
+  domElement.innerHTML = reactElement.children;
+
+  for (const prop in reactElement.props) {
+    domElement.setAttribute(prop, reactElement.props[prop]);
+  }
+
+  root.appendChild(domElement);
+}
+
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit Google"
+};
+
+const root = document.getElementById("root");
+
+customRender(reactElement, root);
