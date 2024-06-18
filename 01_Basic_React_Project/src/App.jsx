df4d2bf05470ff1b52f73676ddb1f96eec368696
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-18 08:09:39 +0500

    Input Container

--- 01_Basic_React_Project/src/App.jsx
+++ 01_Basic_React_Project/src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/Navbar";
+import { Input, Navbar } from "./components";
 
 function App() {
 	const navLinks = ["Home", "About", "Services", "Contact", "Blog"];
@@ -6,7 +6,7 @@ function App() {
 	return (
 		<>
 			<Navbar navLinks={navLinks} />
-			<h1 className="text-3xl bg-green-400 p-2">Hello, React</h1>
+			<Input label="From" />
 		</>
 	);
 }
