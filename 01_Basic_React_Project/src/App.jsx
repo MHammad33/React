ef6fea07fcd86f26f92328b04e8a0430c3bb5871
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-18 15:03:06 +0500

    Components

--- 01_Basic_React_Project/src/App.jsx
+++ 01_Basic_React_Project/src/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { ThemeProvider } from "./contexts/theme";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
 
 function App() {
 	const [themeMode, setThemeMode] = useState("light");
@@ -25,10 +27,12 @@ function App() {
 			<div className="flex flex-wrap min-h-screen items-center">
 				<div className="w-full">
 					<div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-						{/* Theme Buttons */}
+						<ThemeBtn />
 					</div>
 
-					<div className="w-full max-w-sm mx-auto">{/* Card */}</div>
+					<div className="w-full max-w-sm mx-auto">
+						<Card />
+					</div>
 				</div>
 			</div>
 		</ThemeProvider>
