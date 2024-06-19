ae354ad002709ae91588e427c0e5953102ff858c
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-19 17:12:55 +0500

    Add Todo Functionality

--- 01_Basic_React_Project/src/components/TodoForm.jsx
+++ 01_Basic_React_Project/src/components/TodoForm.jsx
@@ -1,10 +1,32 @@
+import { useState } from "react";
+import { useTodo } from "../contexts/TodoContext";
+
 function TodoForm() {
+	const [todo, setTodo] = useState("");
+
+	const { addTodo } = useTodo();
+
+	const handleAddTodo = (e) => {
+		console.log("Adding todo...");
+		e.preventDefault();
+
+		// If todo is empty
+		if (!todo) {
+			alert("Please provide some text to add todo...");
+			return;
+		}
+
+		addTodo({ todo, isCompleted: false });
+	};
+
 	return (
-		<form className="flex">
+		<form className="flex" onSubmit={handleAddTodo}>
 			<input
 				type="text"
 				placeholder="Write Todo..."
 				className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+				value={todo}
+				onChange={(e) => setTodo(e.target.value)}
 			/>
 			<button
 				type="submit"
