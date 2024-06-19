8cb8150166a69e33fa6e6dd0cf8fbeb0adb92ef4
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-19 18:36:25 +0500

    Redux Design
    Created Store
    Created Slice

---
+++ 01_Basic_React_Project/src/features/todo/todoSlice.js
@@ -0,0 +1,29 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [
+    { id: 1, text: "Hello, world" }
+  ],
+}
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const newTodo = {
+        id: nanoid(),
+        text: action.payload.text,
+      }
+      state.todos.push(newTodo);
+    },
+    removeTodo: (state, action) => {
+      const { id } = action.payload;
+      state.todos = state.todos.filter(todo => todo.id !== id);
+    }
+  }
+});
+
+export const { addTodo, removeTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
