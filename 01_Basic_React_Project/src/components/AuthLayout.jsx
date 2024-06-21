ee73ea06f90eb130968043b91b22a23446fa077e
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-21 11:42:31 +0500

    AuthLayout

--- 01_Basic_React_Project/src/components/AuthLayout.jsx
+++ 01_Basic_React_Project/src/components/AuthLayout.jsx
@@ -1,4 +1,27 @@
-function AuthLayout() {
-	return <div>AuthLayout</div>;
+import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+export default function Protected({ children, authentication = true }) {
+	const navigate = useNavigate();
+	const [loading, setLoading] = useState(true);
+	const authStatus = useSelector((state) => state.status);
+
+	useEffect(() => {
+		/** @TODO : Make it more easy to understand */
+		if (authentication && authStatus !== authentication) {
+			navigate("/login");
+		} else if (!authentication && authStatus !== authentication) {
+			navigate("/");
+		}
+		setLoading(false);
+	}, [authStatus, navigate, authentication]);
+
+	return loading ? <h1>Loading...</h1> : <>{children}</>;
 }
-export default AuthLayout;
+
+Protected.propTypes = {
+	children: PropTypes.node.isRequired,
+	authentication: PropTypes.bool,
+};
