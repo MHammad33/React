922febd23820c2c9c5f11e06a52a458f86c39fd1
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-21 17:12:56 +0500

    Routing Defined

--- 01_Basic_React_Project/src/main.jsx
+++ 01_Basic_React_Project/src/main.jsx
@@ -11,9 +11,76 @@ import {
 	createRoutesFromElements,
 } from "react-router-dom";
 
+import { AuthLayout } from "./components/";
+import {
+	AddPost,
+	AllPosts,
+	EditPost,
+	Home,
+	Login,
+	Post,
+	Signup,
+} from "./pages";
+
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path="/" element={<App />}>
+			<Route path="/" element={<Home />} />
+
+			<Route
+				path="/login"
+				element={
+					<AuthLayout authentication={false}>
+						<Login />
+					</AuthLayout>
+				}
+			/>
+
+			<Route
+				path="/signup"
+				element={
+					<AuthLayout authentication={false}>
+						<Signup />
+					</AuthLayout>
+				}
+			/>
+
+			<Route
+				path="/all-posts"
+				element={
+					<AuthLayout authentication>
+						<AllPosts />
+					</AuthLayout>
+				}
+			/>
+
+			<Route
+				path="/add-post"
+				element={
+					<AuthLayout authentication>
+						<AddPost />
+					</AuthLayout>
+				}
+			/>
+
+			<Route
+				path="/edit-post/:slug"
+				element={
+					<AuthLayout authentication>
+						<EditPost />
+					</AuthLayout>
+				}
+			/>
+
+			<Route
+				path="/post/:slug"
+				element={
+					<AuthLayout authentication>
+						<Post />
+					</AuthLayout>
+				}
+			/>
+
 			<Route path="*" element={<h1>404 Not Found</h1>} />
 		</Route>
 	)
--- 01_Basic_React_Project/src/pages/index.js
+++ 01_Basic_React_Project/src/pages/index.js
@@ -1,5 +1,7 @@
 export { default as Signup } from './Signup';
 export { default as Login } from "./Login";
+export { default as Home } from "./Home";
 export { default as AddPost } from "./AddPost";
 export { default as AllPosts } from "./AllPosts";
 export { default as EditPost } from "./EditPost";
+export { default as Post } from "./Post";
