916f96d7cc6f702d127dacd1806b9697a6c412eb
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-21 17:41:15 +0500

    Some todos for future improvements

--- 01_Basic_React_Project/src/store/store.js
+++ 01_Basic_React_Project/src/store/store.js
@@ -3,6 +3,9 @@ import authSlice from "./authSlice";
 
 const store = configureStore({
   reducer: authSlice,
+  /**
+   * @TODO : Add the reducer for the postSlice
+   */
 });
 
 export default store;
