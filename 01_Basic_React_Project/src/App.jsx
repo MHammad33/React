d2aabfbdbf5b4cca3ab852a2f142c468069947f7
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-21 12:40:59 +0500

    Basic use of react hook forms

--- 01_Basic_React_Project/src/App.jsx
+++ 01_Basic_React_Project/src/App.jsx
@@ -1,13 +1,42 @@
-import Navbar from "./components/Navbar";
+import { useForm } from "react-hook-form";
 
 function App() {
-	const navLinks = ["Home", "About", "Services", "Contact", "Blog"];
+	const {
+		register,
+		handleSubmit,
+		watch,
+		reset,
+		formState: { errors },
+	} = useForm();
+
+	const onSubmit = (data) => {
+		console.log(data);
+		reset();
+	};
+
+	console.log(watch("example")); // watch input value by passing the name of it
 
 	return (
-		<>
-			<Navbar navLinks={navLinks} />
-			<h1 className="text-3xl bg-green-400 p-2">Hello, React</h1>
-		</>
+		<form
+			onSubmit={handleSubmit(onSubmit)}
+			className="bg-black text-white h-screen flex flex-col justify-center items-center gap-2"
+		>
+			<input
+				className="px-4 py-2 bg-gray-500 w-1/4 outline-none"
+				type="text"
+				{...register("example")}
+				placeholder="Name"
+			/>
+
+			<input
+				className="px-4 py-2 bg-gray-500 w-1/4 mt-2 outline-none"
+				{...register("exampleRequired", { required: true })}
+				placeholder="email"
+			/>
+			{errors.exampleRequired && <span>This field is required</span>}
+
+			<input type="submit" className="cursor-pointer px-4 py-2 bg-blue-500" />
+		</form>
 	);
 }
 
