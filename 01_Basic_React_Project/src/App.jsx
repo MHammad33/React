69864d8fee0945ba0be8d72c40e8c9e9eebcdbfc
M Hammad Afzal <137163382+MHammad33@users.noreply.github.com>
2024-06-21 12:53:26 +0500

    Example using React Hook Form

--- 01_Basic_React_Project/src/App.jsx
+++ 01_Basic_React_Project/src/App.jsx
@@ -1,10 +1,11 @@
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 
 function App() {
 	const {
 		register,
 		handleSubmit,
-		watch,
+		// watch,
+		control,
 		reset,
 		formState: { errors },
 	} = useForm();
@@ -14,29 +15,63 @@ function App() {
 		reset();
 	};
 
-	console.log(watch("example")); // watch input value by passing the name of it
+	// console.log(watch("example")); // watch input value by passing the name of it
 
 	return (
-		<form
-			onSubmit={handleSubmit(onSubmit)}
-			className="bg-black text-white h-screen flex flex-col justify-center items-center gap-2"
-		>
-			<input
-				className="px-4 py-2 bg-gray-500 w-1/4 outline-none"
-				type="text"
-				{...register("example")}
-				placeholder="Name"
-			/>
+		<div className="w-full px-4 max-w-7xl mx-auto bg-gray-600">
+			<h2 className="text-white font-bold text-4xl p-4">
+				React Hook Form Example
+			</h2>
 
-			<input
-				className="px-4 py-2 bg-gray-500 w-1/4 mt-2 outline-none"
-				{...register("exampleRequired", { required: true })}
-				placeholder="email"
-			/>
-			{errors.exampleRequired && <span>This field is required</span>}
+			<form
+				onSubmit={handleSubmit(onSubmit)}
+				className="text-white h-screen flex flex-col items-center gap-2 w-full"
+			>
+				<div>
+					<label>Name</label>
+					<input
+						className="px-4 py-2 bg-gray-500 w-full outline-none"
+						type="text"
+						{...register("name", { required: "Name is required" })}
+						placeholder="Name"
+					/>
+					{errors.name && <span>{errors.name.message}</span>}
+				</div>
 
-			<input type="submit" className="cursor-pointer px-4 py-2 bg-blue-500" />
-		</form>
+				<div>
+					<label>Email</label>
+
+					<input
+						className="px-4 py-2 bg-gray-500 w-full my-2 outline-none"
+						{...register("email", { required: "Email is required" })}
+						placeholder="Email"
+					/>
+					{errors.email && <span>{errors.email.message}</span>}
+				</div>
+
+				<Controller
+					name="age"
+					control={control}
+					defaultValue=""
+					rules={{ required: "Age is required" }}
+					render={({ field }) => (
+						<div>
+							<label>Age</label>
+							<input
+								className="px-4 py-2 bg-gray-500 w-full my-2 outline-none"
+								{...field}
+								placeholder="Age"
+							/>
+							{errors.age && <span>{errors.age.message}</span>}
+						</div>
+					)}
+				/>
+
+				<button type="submit" className="cursor-pointer px-4 py-2 bg-blue-500">
+					Submit
+				</button>
+			</form>
+		</div>
 	);
 }
 
